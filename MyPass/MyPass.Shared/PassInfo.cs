f56bdddc7cbcae6ac682bfcfc2a145d4b2d8261e
andycnzh <andycnzh@outlook.com>
2015-07-06 23:22:13 +0800

    First Commit

---
+++ MyPass/MyPass.Shared/PassInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyPass
+{
+    class PassInfo
+    {
+        public string PassName { get; set; }
+
+        public string SiteUrl { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+        public string Remark { get; set; }
+
+    }
+}
